93d0e345d3b005a1c9e52414028e4b6109927f11
Jeongho <fgh2585@gamil.com>
2023-07-21 22:09:58 +0900

    CalenderController Calender 수정 메서드 제외하고 작성 GroupController 미미한 수정(상관 안써도 됨)

--- src/main/java/vacationproject/lobster/dto/CalenderResponse.java
+++ src/main/java/vacationproject/lobster/dto/CalenderResponse.java
@@ -1,4 +1,19 @@
 package vacationproject.lobster.dto;
 
+import lombok.Getter;
+import vacationproject.lobster.domain.Calender;
+
+@Getter
 public class CalenderResponse {
+    private String day_start;
+    private String day_end;
+    private String contents;
+    private String calenderOwner;
+
+    public CalenderResponse(Calender calender) {
+        this.day_start = calender.getDay_start();
+        this.day_end = calender.getDay_end();
+        this.contents = calender.getContents();
+        this.calenderOwner = calender.getCalenderOwner();
+    }
 }
