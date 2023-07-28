72d2d9787667f142dcd0091adb004ed742c3d74e
Jeongho <fgh2585@gamil.com>
2023-07-28 11:16:06 +0900

    UpdateRequest 클래스들 생성

---
+++ src/main/java/vacationproject/lobster/dto/UpdateCalenderRequest.java
@@ -0,0 +1,19 @@
+package vacationproject.lobster.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import vacationproject.lobster.domain.User;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateCalenderRequest {
+    private String dayStart;
+    private String dayEnd;
+    private String timeStart;
+    private String timeEnd;
+    private String contents;
+    private User calenderOwner;
+
+}
