37f69ce0159c52208ea368e17f578f53193af62c
Jeongho <fgh2585@gamil.com>
2023-08-03 14:05:47 +0900

    Calendar 오타 수정

--- src/main/java/vacationproject/lobster/domain/Calendar.java
+++ src/main/java/vacationproject/lobster/domain/Calendar.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(name = "calender")
+@Table(name = "calendar")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
