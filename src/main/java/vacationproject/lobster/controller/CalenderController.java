8181fbe2981a787be34812a4339ffd8be48a9196
Jeongho <fgh2585@gamil.com>
2023-08-14 20:31:07 +0900

    Calender controller url 매핑 변경

--- src/main/java/vacationproject/lobster/controller/CalenderController.java
+++ src/main/java/vacationproject/lobster/controller/CalenderController.java
@@ -22,7 +22,7 @@ public class CalenderController {
     private final JwtProvider jwtProvider;
 
     // 일정 생성
-    @PostMapping("/api/calenders/{userId}")
+    @PostMapping("/api/calenders")
     public ResponseEntity<Calender> createEvent(@PathVariable long userId,
                                                 @RequestHeader HttpHeaders headers,
                                                 @RequestBody AddCalenderRequest request) {
@@ -36,9 +36,12 @@ public class CalenderController {
     }
 
     // CalenderOwner id로 해당 사용자의 일정들 가져오기
-    @GetMapping("/api/calenders/{userId}")
-    public ResponseEntity<List<CalenderResponse>> findUserCalenders(@PathVariable long userId) {
-        List<CalenderResponse> userCalenders = calenderService.findUserCalenders(userId)
+    @GetMapping("/api/calenders")
+    public ResponseEntity<List<CalenderResponse>> findUserCalenders(@RequestHeader HttpHeaders headers) {
+        String token = headers.getFirst("Authorization");
+        Long uId = jwtProvider.extractUIdFromToken(token);
+
+        List<CalenderResponse> userCalenders = calenderService.findUserCalenders(uId)
                 .stream()
                 .map(CalenderResponse::new)
                 .toList();
@@ -48,9 +51,12 @@ public class CalenderController {
     }
 
     // 일정 삭제
-    @DeleteMapping("/api/calenders/{userId}/{calenderId}")
-    public ResponseEntity<Void> deleteEvent(@RequestHeader HttpHeaders auth,
+    @DeleteMapping("/api/calenders/{calenderId}")
+    public ResponseEntity<Void> deleteEvent(@RequestHeader HttpHeaders headers,
                                                @PathVariable long calenderId) {
+        String token = headers.getFirst("Authorization");
+        Long uId = jwtProvider.extractUIdFromToken(token);
+
         calenderService.delete(calenderId);
 
         return ResponseEntity.ok()
@@ -58,7 +64,7 @@ public class CalenderController {
     }
 
     // 일정 수정
-    @PutMapping("/api/calenders/{userId}/{calenderId}")
+    @PutMapping("/api/calenders/{calenderId}")
     public ResponseEntity<Calender> updateEvent(@PathVariable Long calenderId,
                                                 @RequestBody UpdateCalenderRequest calenderRequest) {
         Calender updatedEvent = calenderService.update(calenderId, calenderRequest);
